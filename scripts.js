0fcc9dbe0e9adb5b9c01931a0e884f656586b777
Dimitrije <rdomiran@gmail.com>
2022-06-07 15:29:41 +0200

    logicno koriscenje IIFE

--- scripts.js
+++ scripts.js
@@ -3,21 +3,18 @@ const Gameboard = {
 };
 
 
-const displayRender = (() => {
+const render = (() => {
     
     const fieldcontent = () =>{
         Gameboard.gameboard.forEach( (e,i) => {
             let curr_field = document.getElementById(`${i}`);
             curr_field.textContent = e;
-               
         });
     };
 
     return{fieldcontent};
 
 })();
-
-const render = displayRender;
 
 const Player = (name,symbol) =>{
     name = name;
@@ -28,6 +25,5 @@ const Player = (name,symbol) =>{
 console.log(Gameboard);
 const John = Player('Djoka','X');
 console.log(John);
-console.log(render);
 
 render.fieldcontent();
